710267c773df9b92ccb962ad4d5adc1ba226e02d
Michael Myung <michael.h.myung@gmail.com>
2017-11-15 02:58:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // Code your solution in this file
-function findMatching(drivers, name){
-  let matchingDrivers = drivers.filter(function(driver){
-    if (name.toLowerCase() === driver.toLowerCase()){
-      return true
-    } else {
-      return false
-    }
-  })
-  return matchingDrivers;
-}
+//function findMatching(drivers, name){
+  //let matchingDrivers = drivers.filter(function(driver){
+    //if (name.toLowerCase() === driver.toLowerCase()){
+      //return true
+    //} else {
+      //return false
+    //}
+  //})
+  //return matchingDrivers;
+//}
 
+
+
+function findMatching(drivers, string{
+  drivers.filter(function(drivers){return string === drivers.string})
+})
