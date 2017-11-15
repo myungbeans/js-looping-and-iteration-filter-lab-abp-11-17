c53e2d422d2d1eb9c3d56b538dd19fb7b636d44d
Michael Myung <michael.h.myung@gmail.com>
2017-11-15 02:54:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+  let matchingDrivers = drivers.filter(function(driver){
+    if (name.toLowerCase() === driver.toLowerCase()){
+      return true
+    } else {
+      return false
+    }
+  })
+  return matchingDrivers;
+}
+
